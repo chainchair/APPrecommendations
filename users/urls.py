4eb7d10246e1c3872a1c185c0456eb8e53a07740
samuelserpa <sdserpaz@eafit.edu.co>
2026-05-01 23:33:44 -0500

    URLS
    urls added for redirection

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('', views.login_view, name='inicio'),
+    path('registro/', views.registro_view, name='registro'),
+    path('seleccion-intereses/', views.seleccion_intereses_view, name='seleccion_intereses'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    
+]
