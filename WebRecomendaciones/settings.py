ab050cc1cf996be4365c9b9d83f7664017cb0449
samuelserpa <sdserpaz@eafit.edu.co>
2026-05-02 13:58:43 -0500

    BigModel
    Big autofield for more large models

--- WebRecomendaciones/settings.py
+++ WebRecomendaciones/settings.py
@@ -115,5 +115,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
