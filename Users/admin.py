1e8178d8bebb594f12a66abb329a8ee0e85d3cf8
samuelserpa <sdserpaz@eafit.edu.co>
2026-05-01 21:33:07 -0500

    Users and interests

--- Users/admin.py
+++ Users/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import Interest, CustomUser
 
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'interests')
+    search_fields = ('username', 'email')
+    filter_horizontal = ('interests',)
+    
+    def get_queryset(self, request):
+        # Opcional: por defecto muestra solo usuarios normales
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.filter(is_staff=False)
+        return qs
+
+
 admin.site.register(Interest)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
