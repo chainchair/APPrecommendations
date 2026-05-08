fbd5e61711d397a3d8ec7e1c91b93cd49c7a8fa7
samuelserpa <sdserpaz@eafit.edu.co>
2026-05-08 10:35:02 -0500

    config

--- WebRecomendaciones/settings.py
+++ WebRecomendaciones/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #custom apps
     'users.apps.UsersConfig',
-    'places.apps.PlacesConfig',
+    'places',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
